ad1a005f1f14c4ba58b5bce7d9649cf0cd587420
wanghx12345 <165381911+wanghx12345@users.noreply.github.com>
2024-04-10 22:54:45 +0800

    Add files via upload

---
+++ UnitTest2.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int sum = 21;
+			int n = 6;
+			int str1[6] = { 1,2,3,4,5,6 };
+			Assert::AreEqual(sum, maxSubArraySum(str1,n));
+		}
+		TEST_METHOD(TestMethod2)
+
+		{
+			int sum = 0;
+			int n = 5;
+			int str2[6] = { -1,-2,-3,-4,-5,-6 };
+			Assert::AreEqual(sum, maxSubArraySum(str2,n));
+
+		}
+	};
+}
